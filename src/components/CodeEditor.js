eb8973243ac461c99f248d076483910f3b57f9c4
Andreas <info@azettl.net>
2024-09-25 22:40:29 +0200

    add collapsed and fullscreen state

--- src/components/CodeEditor.js
+++ src/components/CodeEditor.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Editor from '@monaco-editor/react';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const CodeEditor = () => {
   const [html, setHtml] = useState('Enter your HTML here');
   const [css, setCss] = useState('@page{ margin: 2cm; }');
   const [js, setJs] = useState('// Enter your JavaScript here');
   const previewRef = useRef(null);
+  const [editorStates, setEditorStates] = useState({
+    html: { collapsed: false, fullscreen: false },
+    css: { collapsed: false, fullscreen: false },
+    js: { collapsed: false, fullscreen: false }
+  });
 
   useEffect(() => {
     updatePreview();
@@ -39,54 +45,56 @@ const CodeEditor = () => {
     }
   };
 
+  const toggleEditorState = (editor, state) => {
+    setEditorStates(prevStates => ({
+      ...prevStates,
+      [editor]: {
+        ...prevStates[editor],
+        [state]: !prevStates[editor][state]
+      }
+    }));
+  };
+
+  const renderEditor = (type, value, setValue) => (
+    <div className={`flex-1 flex flex-col ${editorStates[type].fullscreen ? 'fixed inset-0 z-50 bg-gray-800' : ''}`}>
+      <div className="box-heading flex justify-between items-center p-2">
+        <span>{type.toUpperCase()}</span>
+        <div>
+          <button onClick={() => toggleEditorState(type, 'collapsed')} className="mr-2 p-1">
+            <i className={`bi ${editorStates[type].collapsed ? 'bi-chevron-up' : 'bi-chevron-down'}`}></i>
+          </button>
+          <button onClick={() => toggleEditorState(type, 'fullscreen')} className="p-1">
+            <i className={`bi ${editorStates[type].fullscreen ? 'bi-fullscreen-exit' : 'bi-fullscreen'}`}></i>
+          </button>
+        </div>
+      </div>
+      <div className={`flex-1 ${editorStates[type].collapsed ? 'hidden' : ''}`}>
+        <Editor
+          height="100%"
+          defaultLanguage={type}
+          value={value}
+          onChange={setValue}
+          theme="vs-dark"
+          options={{ minimap: { enabled: false } }}
+        />
+      </div>
+    </div>
+  );
+
   return (
     <div className="flex flex-col h-screen bg-gray-800 text-white">
       <div className="flex-1 flex">
         <div className="w-1/2 flex flex-col">
-          <div className="flex-1 flex flex-col">
-            <div className="box-heading">HTML</div>
-            <div className="flex-1">
-              <Editor
-                height="100%"
-                defaultLanguage="html"
-                value={html}
-                onChange={setHtml}
-                theme="vs-dark"
-                options={{ minimap: { enabled: false } }}
-              />
-            </div>
-          </div>
-          <div className="flex-1 flex flex-col">
-            <div className="box-heading">CSS</div>
-            <div className="flex-1">
-              <Editor
-                height="100%"
-                defaultLanguage="css"
-                value={css}
-                onChange={setCss}
-                theme="vs-dark"
-                options={{ minimap: { enabled: false } }}
-              />
-            </div>
-          </div>
-          <div className="flex-1 flex flex-col">
-            <div className="box-heading">JavaScript</div>
-            <div className="flex-1">
-              <Editor
-                height="100%"
-                defaultLanguage="javascript"
-                value={js}
-                onChange={setJs}
-                theme="vs-dark"
-                options={{ minimap: { enabled: false } }}
-              />
-            </div>
-          </div>
+          {renderEditor('html', html, setHtml)}
+          {renderEditor('css', css, setCss)}
+          {renderEditor('js', js, setJs)}
         </div>
         <div className="w-1/2 p-4 flex flex-col">
-          <div className="box-heading">
-            Preview <i>powered by paged.js</i>
-            <button className="reload" onClick={updatePreview}>Reload</button>
+          <div className="box-heading flex justify-between items-center p-2">
+            <span>Preview <i>powered by paged.js</i></span>
+            <button className="reload p-1" onClick={updatePreview}>
+              <i className="bi bi-arrow-clockwise"></i> Reload
+            </button>
           </div>
           <div className="flex-1">
             <iframe
